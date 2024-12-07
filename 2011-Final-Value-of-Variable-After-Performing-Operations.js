77e600e82189506abcb98c7974facb9e528a5902
Nikhil Mishra <codewithniku@gmail.com>
2024-12-07 20:05:49 +0530

    [2011] [Time Beats: 100.00%] [Memory Beats: 30.95%] - LeetPush

---
+++ 2011-Final-Value-of-Variable-After-Performing-Operations.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let x = 0
+    
+    for(i=0;i<operations.length;i++){
+        if(operations[i] == "X++" || operations[i] == "++X"){
+            x += 1
+        }
+        if(operations[i] == "X--" || operations[i] == "--X"){
+            x -= 1
+        }
+    }
+
+    return x
+
+};
